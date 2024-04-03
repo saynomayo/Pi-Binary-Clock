de096cb941d8e9183c2f41f505a66ca1029861de
mwaples <mwaples@udel.edu>
2024-04-03 15:42:46 -0400

    Updated all necessary files with basic code and information

---
+++ display.h
@@ -0,0 +1,13 @@
+int open_display(void);
+
+void display_time(int hours, int minutes, int seconds);
+
+void display_colons(void);
+
+void display_hours(int hours);
+
+void display_minutes(int minutes);
+
+void display_seconds(int seconds);
+
+void close_display(void);
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "display.h"
+#include <stdio.h>
+
+int main(void) {
+return 0;
+}
