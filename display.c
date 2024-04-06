97b7ae210671887cb88e6a72e028242f40dfef94
rdaluz <rdaluz@udel.edu>
2024-04-06 12:58:49 -0400

    Coded display_hours function so that it converts hours to decimal and displays hours in binary onto sense hat.

--- display.c
+++ display.c
@@ -6,6 +6,10 @@
 #define WHITE 0xFFFF
 #define BLACK 0x0000
 
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+
 int open_display(void);
 
 void display_time(int hours, int minutes, int seconds);
@@ -17,7 +21,7 @@ void display_colons(void);
 	bm->pixel[5][2]=WHITE;
 	bm->pixel[5][4]=WHITE;
 	bm->pixel[5][5]=WHITE;
-	bm->pixel[4][1]=WHITE;
+
 	bm->pixel[4][2]=WHITE;
 	bm->pixel[4][4]=WHITE;
 	bm->pixel[4][5]=WHITE;
@@ -32,7 +36,57 @@ void display_colons(void);
 	bm->pixel[1][4]=WHITE;
 	bm->pixel[1][5]=WHITE;
 
-void display_hours(int hours);
+void display_hours(int hours) {
+
+// Converting the hour to binary 
+
+	int binary_hours = 0 ;
+	int i = 1 ;
+	
+	while (hours > 0) {
+		int remainder = hours % 2 ;
+
+		binary_hours += remainder * i ;
+		
+		hours = hours / 2 ;
+
+		i = i * 10 ;
+	}
+
+}
+
+// Displaying Hours
+	// Check for_00001
+	if ( binary_hours[4] == 1 ) { 
+		bm->pixel[6][7]=BLUE ;
+	} else {
+		bm->pixel[6][7]=BLACK ;
+	}
+
+	// Check for_00010
+	if (binary_hours[3] == 1 ) {
+		bm->pixel[6][6]=BLUE ;
+	} else {
+		bm->pixel[6][6]=BLACK ;
+
+	// Check for_00100
+	if (binary_hours[2] == 1) {
+		bm->pixel[6][5]=BLUE ;
+	} else {
+		bm->pixel[6][5]=BLACK ;
+
+	//Check for_01000
+	if (binary_hours[1] == 1) {
+		bm->pixel[6][4]=BLUE ;
+	} else {
+		bm->pixel[6][4]=BLACK ;
+
+	//Check for_10000
+	if (binary_hours[0] == 1) {
+		bm->pixel[6][3]=BLUE ;
+	} else {
+		bm->pixel[6][3]=BLACK ;
+
 
 void display_minutes(int minutes);
 
