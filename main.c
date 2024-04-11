d69ee182a3beebafc320da68a59687636867d737
Mekhai Waples <mwaples@ubuntu-1804.udel.edu>
2024-04-10 23:37:26 -0400

    fixed comments in main.c

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include "display.h"
 
 int main(void) {
-// currently (as of 4/7/24, 4:06pm) none of the conditions required for open_display
-// or close_display are programmed yet. This is because I was simply testing
-// the functions.
 	int hours;
 	int minutes;
 	int seconds;
@@ -12,14 +9,11 @@ int main(void) {
 	scanf("%d:%d:%d", &hours, &minutes, &seconds);
 	//scan time from stdin, skip over colons and assign
 	//everything relatively
-	//while time is 8 character
-	//continue scanning time
+	//while time is "proper"
 	while (hours>-1 && hours<24 && minutes>-1 && minutes<60 && seconds>-1 && seconds<60) {
 		scanf("%d:%d:%d", &hours, &minutes, &seconds);
 		display_time(hours, minutes, seconds);
 	}
-	//display the time
-	//wait one second
 	//exit program
 	close_display();
 	return 0;
